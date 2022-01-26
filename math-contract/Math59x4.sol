e7d73823aca2dbcf36c1550ecb2ee3aea2c7ec95
tuzijun111 <58006108+tuzijun111@users.noreply.github.com>
2022-01-26 11:03:23 -0800

    Update Math59x4.sol

--- math-contract/Math59x4.sol
+++ math-contract/Math59x4.sol
@@ -5,7 +5,7 @@ import "./PRBMath.sol";
 
 /// @title PRBMathSD59x18
 /// @author Paul Razvan Berg
-/// @notice Smart contract library for advanced fixed-point math that works with int256 numbers considered to have 18
+/// @notice Smart contract library for advanced fixed-point math that works with int256 numbers considered to have 4
 /// trailing decimals. We call this number representation signed 59.18-decimal fixed-point, since the numbers can have
 /// a sign and there can be up to 59 digits in the integer part and up to 18 decimals in the fractional part. The numbers
 /// are bound by the minimum and the maximum values permitted by the Solidity type int256.
