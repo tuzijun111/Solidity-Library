8adaabdfc13145c913e22a123ce0e4b472056de0
tuzijun111 <58006108+tuzijun111@users.noreply.github.com>
2022-01-26 11:43:27 -0800

    Update Math59x2.sol

--- math-contract/Math59x2.sol
+++ math-contract/Math59x2.sol
@@ -494,7 +494,7 @@ library Math59x2 {
                 sign = -1;
                 // Do the fixed-point inversion inline to save gas. The numerator is SCALE * SCALE.
                 assembly {
-                    x := div(SCALE * SCALE, x)
+                    x := div(10000, x)
                 }
             }
 
