d84c2e1dbfef611f5ea18d1717403247091089eb
tuzijun111 <58006108+tuzijun111@users.noreply.github.com>
2022-01-26 10:58:39 -0800

    Update Math59x4.sol

--- math-contract/Math59x4.sol
+++ math-contract/Math59x4.sol
@@ -9,7 +9,7 @@ import "./PRBMath.sol";
 /// trailing decimals. We call this number representation signed 59.18-decimal fixed-point, since the numbers can have
 /// a sign and there can be up to 59 digits in the integer part and up to 18 decimals in the fractional part. The numbers
 /// are bound by the minimum and the maximum values permitted by the Solidity type int256.
-library PRBMathSD59x18 {
+library Math59x4 {
     /// @dev log2(e) as a signed 59.18-decimal fixed-point number.
     int256 internal constant LOG2_E = 1_442695040888963407;
 
